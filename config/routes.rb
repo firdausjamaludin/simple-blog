f5ae4b624f18816ad85f57658c47066a7b166fc5
firdausjamaludin <firdausjmldn@gmail.com>
2022-07-18 16:40:14 +0800

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  
   devise_for :authors
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'posts#index'
+  root to: "home#index"
   scope module: 'authors' do
     resources :posts
   end
